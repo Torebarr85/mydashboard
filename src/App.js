3b59d8dd6bdb201578385dc6b99cb10e372f1232
Salvatore <torebarr85@github.com>
2023-11-06 21:39:12 +0100

    navbar ok

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ const App = () => {
   const activeMenu = useStateContext();
 
   return (
-    <>
+    <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="settings" position="top">
               <button
                 type="button"
-                className="text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray"
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                 style={{ background: "blue", borderRadius: "50%" }}>
                 <FiSettings />
               </button>
@@ -44,7 +44,7 @@ const App = () => {
           </div>
 
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
@@ -52,47 +52,49 @@ const App = () => {
               <Sidebar />
             </div>
           )}
-
           <div
-            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
-              ${activeMenu ? "md:ml-72" : "flex-2"}`}>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
-          </div>
 
-          <div>
-            <Routes>
-              {/* dashboard  */}
-              <Route path="/" element={<Ecommerce />} />
-              <Route path="/ecommerce" element={<Ecommerce />} />
+            <div>
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
 
-              {/* pages  */}
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/employees" element={<Employees />} />
-              <Route path="/customers" element={<Customers />} />
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
 
-              {/* apps  */}
-              <Route path="/kanban" element={<Kanban />} />
-              <Route path="/editor" element={<Editor />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/color-picker" element={<ColorPicker />} />
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
 
-              {/* charts  */}
-              <Route path="/line" element={<Line />} />
-              <Route path="/area" element={<Area />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/financial" element={<Financial />} />
-              <Route path="/color-mapping" element={<ColorMapping />} />
-              <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} />
-            </Routes>
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
-    </>
+    </div>
   );
 };
-
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,8 @@ import { links } from "../data/dummy.js";
 import { useStateContext } from "../contexts/ContextProvider.js";
 
 const Sidebar = () => {
+  const currentColor = "black";
+
   const { activeMenu, setActiveMenu } = useStateContext();
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -16,25 +18,26 @@ const Sidebar = () => {
     /*overflow hidden = se un contenuto eccede il contenitore allora nascondilo. solo in caso di md media query 768px 
       overflow auto = invece mette una barra di scorrimento se il contenuto eccede... 
       md:hover:overflow-auto= fa vedere la barra solo se vado sopra (hover appunto)*/
+    /*In questo caso, il menu verrà mostrato solo se activeMenu è vero. Se
+      activeMenu è falso, il menu non verrà renderizzato nel DOM */
+
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {/*In questo caso, il menu verrà mostrato solo se activeMenu è vero. Se activeMenu è falso, il menu non verrà renderizzato nel DOM */}
       {activeMenu && (
         <>
           {/* LOGO + PULSANTE CHIUDI */}
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
               <SiShopware /> <span>ESSEBI</span>
             </Link>
+
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
-                <MdOutlineCancel />
-              </button>
+                style={{ color: currentColor }}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block:md:hidden"></button>
             </TooltipComponent>
           </div>
 
